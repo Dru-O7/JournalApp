951dd4929e1eb0c32dbad434c8da716101490f2b
Dru-O7 <e21cseu0291@bennett.edu.in>
2025-01-29 11:21:10 +0530

    first commit

---
+++ src/main/java/net/engineeringdigest/journalApp/controller/AdminController.java
@@ -0,0 +1,47 @@
+package net.engineeringdigest.journalApp.controller;
+
+import net.engineeringdigest.journalApp.entity.User;
+import net.engineeringdigest.journalApp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/all-users")
+    public ResponseEntity<?> getAllUsers(){
+        List<User> all=userService.getAll();
+        if(all!=null && !all.isEmpty()){
+            return new ResponseEntity<>(all, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/create-admin-user")
+    public void createUser(@RequestBody User user){
+        userService.saveAdmin(user);
+    }
+
+    @PutMapping("/update-admin-user")
+    public ResponseEntity<?> updateAdmin(@RequestBody User user){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userName = authentication.getName();
+        User userInDb = userService.finduserByUserName(userName);
+        userInDb.setUserName(user.getUserName());
+        userInDb.setPassword(user.getPassword());
+        userService.saveAdmin(userInDb);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+    }
+}
