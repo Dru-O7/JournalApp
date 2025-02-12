c6f97f414c9118583d7642c679d753baf1d832ff
Dru-O7 <e21cseu0291@bennett.edu.in>
2025-02-12 15:38:25 +0530

    Added field for email and sentiment analysis and API cache

--- src/main/java/net/engineeringdigest/journalApp/service/QuoteService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/QuoteService.java
@@ -1,6 +1,7 @@
 package net.engineeringdigest.journalApp.service;
 
 import net.engineeringdigest.journalApp.api.QuoteResponse;
+import net.engineeringdigest.journalApp.cache.AppCache;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -12,9 +13,13 @@ import org.springframework.web.client.RestTemplate;
 public class QuoteService {
     @Autowired
     private RestTemplate restTemplate;
-    private static final String API="https://stoic.tekloon.net/stoic-quote";
 
+    @Autowired
+    private AppCache appCache;
+
+//    private static final String API="https://stoic.tekloon.net/stoic-quote";
     public QuoteResponse getQuote(){
+        String API=appCache.appCache.get("quote_api");
         ResponseEntity<QuoteResponse> response = restTemplate.exchange(API, HttpMethod.GET, null, QuoteResponse.class);
         QuoteResponse body = response.getBody();
         return body;
--- src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
@@ -2,6 +2,8 @@ package net.engineeringdigest.journalApp.service;
 
 
 import net.engineeringdigest.journalApp.api.WeatherResponse;
+import net.engineeringdigest.journalApp.cache.AppCache;
+import net.engineeringdigest.journalApp.repository.ConfigJournalAppRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
@@ -16,14 +18,16 @@ public class WeatherService {
     @Value("${weather.api.key}")
     private String apiKey;
 
-    private static final String API="http://api.weatherstack.com/current?access_key=KEY&query=CITY";
+//    private static final String API="http://api.weatherstack.com/current?access_key=KEY&query=CITY";
 
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private AppCache appCache;
 
     public WeatherResponse getWeather(String city){
-        String finalApi=API.replace("CITY",city).replace("KEY",apiKey);
+        String finalApi=appCache.appCache.get("weather_api").replace("<city>",city).replace("<apiKey>",apiKey);
         ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalApi, HttpMethod.GET, null, WeatherResponse.class);
         WeatherResponse body = response.getBody();
         return body;
